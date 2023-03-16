860c34f5f2d181f917a536279710108c4d274250
Ali Jafari <alijfri99@gmail.com>
2023-03-16 18:05:46 +0330

    Fixed a bug in read_dataset_images

--- usps_jpg.py
+++ usps_jpg.py
@@ -4,7 +4,10 @@ from PIL import Image
 def read_dataset_images(path):
     with open(path, 'r') as file:
         images = file.read()
-        images = images.split(' ')
+        images = images.split('\n')
     return images
 
 
+x = read_dataset_images('usps_training.txt')
+print(len(x))
+print(x[10])
