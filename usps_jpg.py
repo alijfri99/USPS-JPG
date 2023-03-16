e67bb1e7a465830f403ec146fae6e97e1b5e21f6
Ali Jafari <alijfri99@gmail.com>
2023-03-16 19:05:45 +0330

    Finished the code and extracted the datasets.

--- usps_jpg.py
+++ usps_jpg.py
@@ -1,8 +1,9 @@
 import numpy
 from PIL import Image
+import os
 import matplotlib.pyplot as plt
 
-def read_dataset_images(path):
+def read_dataset(path):
     images = ""
     with open(path, 'r') as file:
         images = file.read()
@@ -12,11 +13,32 @@ def read_dataset_images(path):
 
 def image_to_numpy_array(input_image):
     input_image = input_image.split(' ')
-    output_image = numpy.zeros((16, 16), dtype=numpy.float32)
-    output_class = input_image[0]
+    output_image = numpy.zeros((16, 16), dtype=numpy.uint8)
+    output_class = str(int(input_image[0]) - 1)
     
     for i in range(1, 257):
         current_pixel_value = float(input_image[i].split(':')[1])
+        current_pixel_value = int((current_pixel_value + 1) * 255 / 2)
         output_image[(i - 1) // 16][(i - 1) % 16] = float(current_pixel_value)
 
     return output_image, output_class
+
+def save_image(image, image_class, dataset_type, file_name):
+    path = os.path.join('./Dataset', dataset_type, image_class, str(file_name) + '.jpg')
+    image = Image.fromarray(image)
+    image.save(path)
+
+def extract_dataset(dataset_name):
+    dataset = read_dataset(dataset_name)
+    dataset_type = dataset_name.split('_')[1][:-4]
+    print('Extracting ' + dataset_type + '.')
+
+    for counter in range(len(dataset) - 1):
+        image, image_class = image_to_numpy_array(dataset[counter])
+        save_image(image, image_class, dataset_type, counter)
+
+    print(dataset_type + ": " + str(counter))
+
+extract_dataset('usps_training.txt')
+extract_dataset('usps_testing.txt')
+print("Done!")
